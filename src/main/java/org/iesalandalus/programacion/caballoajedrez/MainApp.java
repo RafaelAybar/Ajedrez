ece0421c010409b8d93106fe7dd2dedad3a7db1f
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-10-24 10:48:50 +0200

    Creado el enum e instanciado

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/MainApp.java
@@ -8,5 +8,10 @@ public class MainApp {
 	
 	public static void main(String[] args) {
 		System.out.println("Programa para aprender a colocar y mover un caballo en el tablero de ajedrez");
+		
+	}
+	//Creanis ek enum
+	public enum Color {
+		BLANCO, NEGRO
 	}
 }
