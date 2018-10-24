06d746285509ace09c9394373757ab03e9f85df3
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-10-24 11:05:32 +0200

    Añadido enum Direccion e instanciación del mismo

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/MainApp.java
@@ -10,8 +10,27 @@ public class MainApp {
 		System.out.println("Programa para aprender a colocar y mover un caballo en el tablero de ajedrez");
 		
 	}
-	//Creanis ek enum
+	//Creamos el enum Color
 	public enum Color {
 		BLANCO, NEGRO
 	}
+	
+	//Instanciamos Color
+	Color blanco = Color.BLANCO;
+	Color negro = Color.NEGRO;
+	
+	//Creamos el enum Direccion
+	public enum Direccion{
+		ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_DERECHA,  ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA, IZQUIERDA_ABAJO
+	}
+	//Instanciamos Direccion
+	Direccion arriba_izquierda = Direccion.ARRIBA_IZQUIERDA;
+	Direccion arriba_derecha = Direccion.ARRIBA_DERECHA;
+	Direccion derecha_arriba = Direccion.DERECHA_ARRIBA;
+	Direccion derecha_abajo = Direccion.DERECHA_ABAJO;
+	Direccion abajo_derecha = Direccion.ABAJO_DERECHA;
+	Direccion abajo_izquierda = Direccion.ABAJO_IZQUIERDA;
+	Direccion izquierda_arriba = Direccion.IZQUIERDA_ARRIBA;
+	Direccion izquierda_abajo = Direccion.IZQUIERDA_ABAJO;
+	
 }
