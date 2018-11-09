88dbdab65e6b7084eafe1f89e9aa7fcd4ddc4ea0
Rafael <rafaelaybar@hotmail.com>
2018-11-09 11:21:52 +0100

    Creada la clase caballo

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -1,10 +1,12 @@
 package org.iesalandalus.programacion.caballoajedrez;
+import org.iesalandalus.programacion.caballoajedrez.Posicion;
 
 public class Caballo {
+	String color = Posicion.toString();
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+	
+	}*/
 
 }
