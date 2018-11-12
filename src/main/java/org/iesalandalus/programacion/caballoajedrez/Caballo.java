d7c3677962a182e57c7c6d59a45547f9a4c1885c
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-11-12 12:07:20 +0100

    Refactorización de código. Método equals correcto

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -1,26 +1,22 @@
 package org.iesalandalus.programacion.caballoajedrez;
-import org.iesalandalus.programacion.caballoajedrez.Posicion;
-import org.iesalandalus.programacion.caballoajedrez.Color;
 
 public class Caballo {
-	char columna;
-	int fila;
-	String posicion = (new Posicion(fila, columna)).toString();;
+	private char columna;
+	private int fila;
+	String posicion = (new Posicion(fila, columna)).toString();
 	Color color;
-	
-	//Creamos el setter para color
-		public void setColor(String color) {
-			/*ValueOf nos dará una excepción IllegalArgumentException si el color 
-			que hemos introducido NO está contemplado en el enum que hemos definido*/
-			 this.color = Color.valueOf(color);
-			 }
-		//Creamos el getter para el color
-		public Color getColor(Color color) {
-			return color;
-			}
-		//Creamos el setter para posicion
-		public void setPosicion(String posicion) {
-			this.posicion = posicion;
-			}
-		
-		}
+
+	// Creamos el setter para color
+	public void setColor(String color) {
+		/**
+		 * ValueOf nos dará una excepción IllegalArgumentException si el color que hemos
+		 * introducido NO está contemplado en el enum que hemos definido
+		 */
+		this.color = Color.valueOf(color);
+	}
+
+	// Creamos el getter para el color
+	public Color getColor(Color color) {
+		return color;
+	}
+}
--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -1,59 +1,73 @@
 package org.iesalandalus.programacion.caballoajedrez;
 
 public class Posicion {
-			private int fila;
-			private char columna;
-			
-			// Creamos los getter
-			public char getColumna() {
-				return columna;
-			}
-			public int getFila() {
-				return fila;
-			}
-			
-			//Creamos los setter
-			public void setColumna(char columna) {
-				if (columna < 'a' || columna > 'h') throw new IllegalArgumentException("Los valores de la columna son incoherentes");
-				else {
-					this.columna = columna;
-				}
-				
-			}
-			public void setFila(int fila) {
-				if (fila < 0 && fila > 8)throw new IllegalArgumentException("Los valores de la fila son incoherentes");	
-				else {
-					this.fila = fila;
-				}
-			}
-			//Creamos el constructor
-			public Posicion(int fila, char columna){
-				setFila(fila);
-				setColumna(columna);
-				
-				/*if (columna < 'a' || columna > 'h') throw new IllegalArgumentException("Los valores de la columna son incoherentes");
-				else {
-					this.columna = columna;
-				}
-				if (fila < 0 && fila > 8)throw new IllegalArgumentException("Los valores de la fila son incoherentes");	
-				else {
-					this.fila = fila;
-				}*/
-			}
-			//Creamos el constructor copia
-			public Posicion(final Posicion copia) {
-				this.columna = copia.columna;
-				this.fila = copia.columna;
-			}
-			//Creamos el método equals, como no se usará en principio queda comentado
-				//cadena1ejemplo.equals(cadena2ejemplo);
-			
-			//Creamos el método toString
-			@Override
-			public String toString(){
-				//this.fila = getFila();
-				//this.columna = getColumna();
-				//return "[fila][columna]";
-				return "getFila()"+ "getColumna()";
-			}
-					}
+	private int fila;
+	private char columna;
+
+	// Creamos los getter
+	public char getColumna() {
+		return columna;
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	// Creamos los setter
+	public void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("Los valores de la columna son incoherentes");
+		} else {
+			this.columna = columna;
+		}
+
+	}
+
+	public void setFila(int fila) {
+		if (fila < 0 && fila > 8) {
+			throw new IllegalArgumentException("Los valores de la fila son incoherentes");
+		} else {
+			this.fila = fila;
+		}
+	}
+
+	// Creamos el constructor
+	public Posicion(int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+	}
+
+	// Creamos el constructor copia
+	public Posicion(final Posicion copia) {
+		this.columna = copia.columna;
+		this.fila = copia.columna;
+	}
+
+	// Creamos el método equals
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Posicion other = (Posicion) obj;
+		if (columna != other.columna) {
+			return false;
+		}
+		if (fila != other.fila) {
+			return false;
+		}
+		return true;
+	}
+
+	// Creamos el método toString
+	@Override
+	public String toString() {
+		return getColumna() + "" + getFila();
+	}
+}
