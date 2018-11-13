fcf7b180b302e11078ac5ca572fd6b5b5ee09fa4
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-11-13 12:42:09 +0100

    Creados los constructores con las posiciones por defecto, y se asigna el color según la fila

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.caballoajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Caballo {
 	Posicion posicion;
 	Color color;
@@ -30,6 +32,34 @@ public class Caballo {
 
 	// Creamos el constuctor con el valor por defecto '8b'
 	public Caballo(Posicion posicion) {
-		this.posicion = new Posicion(8, 'a');
+		this.posicion = new Posicion(8, 'b');
+	}
+
+	/*
+	 * Creamos el constructor que establece las posiciones por defecto según el
+	 * color
+	 */
+	public Caballo(Color color) {
+		if (color.equals(Color.BLANCO)) {
+			this.posicion = new Posicion(1, 'b');
+		} else {
+			this.posicion = new Posicion(8, 'b');
+		}
+	}
+
+	// Creamos el constructor con el color y la fila inicial como parámetros
+	public Caballo(Color color, int columna) throws OperationNotSupportedException {
+		// Comprobamos que la fila sea la correcta
+		if (columna != 'b' || columna != 'g') {
+			throw new OperationNotSupportedException("La fila introducida no es correcta");
+		}
+		// Asignamos el color en función de la fila introducida
+		if (columna == 'b') {
+			this.color = Color.BLANCO;
+			this.posicion = new Posicion(1, 'b');
+		} else {
+			this.color = Color.NEGRO;
+			this.posicion = new Posicion(1, 'g');
+		}
 	}
 }
